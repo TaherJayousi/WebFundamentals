b7345db74692cd24c0d6c49d8d17a65383d67394
TaherJayousi <taher1994193@gmail.com>
2022-08-29 10:16:41 +0300

    final commet

---
+++ JS/Fizzbuzz/Fizzbuzz.js
@@ -0,0 +1,16 @@
+var x;
+for(var i=1; i<=100;i++){
+    if(i%3==0&&i%5==0){
+        x="FizzBuzz"
+    }
+    else if(i%3==0){
+        x="Fizz";
+    }
+    else if(i%5==0){
+        x="Buzz";
+    }
+    else{
+        x=i;
+    }
+    console.log(x);
+}
