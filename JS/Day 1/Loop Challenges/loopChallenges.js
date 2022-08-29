c69d5286960cb2501d8d1de9d8618fb7c60ed5cb
TaherJayousi <taher1994193@gmail.com>
2022-08-29 11:43:48 +0300

    final commet

---
+++ JS/Day 1/Loop Challenges/loopChallenges.js
@@ -0,0 +1,33 @@
+for(var i=1; i<=20;i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+for(var x=100; x>0;x--){
+    if(x%3==0){
+        console.log(x);
+    }
+}
+
+var y=4;
+while(y > -5){
+    console.log(y);
+    y-=1.5;
+}
+
+var z=1;
+var sum=0;
+while(z <=100){
+    sum+=z;
+    z++;
+}
+console.log(sum);
+
+var p=1;
+var product=1;
+while(p<=12){
+    product = product * p;
+    p++;
+}
+console.log(product);
